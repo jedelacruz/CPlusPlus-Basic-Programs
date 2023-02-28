c2fb0802437faad14a9228914af8fb5ef52fdcae
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-02-28 09:44:18 +0800

    Create stringConcatenation.cpp

---
+++ stringConcatenation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string firstName = "Je";
+    string lastName = "Dela Cruz";
+
+    string info ="I am " + firstName + " " + lastName;
+
+    cout << info;
+
+
+    return 0;
+}
+
