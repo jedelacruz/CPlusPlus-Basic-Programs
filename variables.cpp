abbdaf1cad02b19c2a7115615dc44e10aa25bd02
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-02-28 09:39:36 +0800

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string name = "Je Dela Cruz";
+    int age = 16;
+    double weight = 49.5;
+
+    cout << name << endl;
+    cout << age << endl;
+    cout << weight;
+  
+    return 0;
+}
