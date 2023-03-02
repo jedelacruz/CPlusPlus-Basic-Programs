a064b5c6773cea4280a1db1c83120269055a52fe
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-02 18:22:57 +0800

    Create const.cpp

---
+++ const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // you cannot change the value later on when you use const
+    const string je = "name";
+
+    cout << je;
+
+    return 0;
+}
+
