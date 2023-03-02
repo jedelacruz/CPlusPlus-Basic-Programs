eafb851f4f412ffe064850517cd274068bea623d
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-02 18:33:56 +0800

    Create additionMathExpression.cpp

---
+++ additionMathExpression.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   // mathematical expressions ( ADDITION )
+
+   int a = 12;
+   int b = 9;
+   int c = a + b;
+
+   cout << c;
+
+    return 0;
+}
+
