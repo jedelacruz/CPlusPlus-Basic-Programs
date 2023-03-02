7e3bd1eaa10c6bd5998977689983152c8f095e82
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-02 18:38:18 +0800

    Create modulusDivMathExpression.cpp

---
+++ modulusDivMathExpression.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   // mathematical expressions ( MODULUS DIVISION )
+
+   int a = 9;
+   int b = 2;
+   int c = a % b;
+
+   cout << c;
+
+    return 0;
+}
+
