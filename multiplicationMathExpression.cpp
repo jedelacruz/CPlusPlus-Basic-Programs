e7ff31587535154bb2bc4ac147b2f922faec994d
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-02 18:37:13 +0800

    Create multiplicationMathExpression.cpp

---
+++ multiplicationMathExpression.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   // mathematical expressions ( MULTIPLICATION )
+
+   int a = 7;
+   int b = 4;
+   int c = a * b;
+
+   cout << c;
+
+    return 0;
+}
+
