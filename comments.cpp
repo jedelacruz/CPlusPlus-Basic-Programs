cd68ee19dc72adfd59690e46a316260b6ae0b048
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-02 18:26:50 +0800

    Create comments.cpp

---
+++ comments.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "COMMENT" << endl;
+    cout << "Everything inside the comment will not run" << endl;
+    cout << "They are use when you are adding description to your code" << endl;
+    cout << "To use a comment you can use /*   */ for multi line comment and // for single line comment" << endl;
+
+    /*
+
+    I am Handsome
+
+    */
+
+    // I love you
+
+
+    return 0;
+}
+
