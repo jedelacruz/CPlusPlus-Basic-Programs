0b6abaf475b3a4f14de38adb0a47ac2427e533bb
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-02 18:36:09 +0800

    Create divisionMathExpression.cpp

---
+++ divisionMathExpression.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   // mathematical expressions ( DIVISION )
+
+   int a = 9;
+   int b = 3;
+   int c = a / b;
+
+   cout << c;
+
+    return 0;
+}
+
