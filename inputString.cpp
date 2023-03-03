2a4697556eada97727389ab54d54262bb8b1290b
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-03 19:02:22 +0800

    Create inputString.cpp

---
+++ inputString.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   string name;
+   cout << "Enter your name: ";
+   cin >> name;
+   cout << "You are " + name;
+
+
+    return 0;
+}
+
