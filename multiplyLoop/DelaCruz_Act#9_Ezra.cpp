df6049869953f2730442335295ac41377dd49747
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-05-04 08:39:15 +0800

    Create DelaCruz_Act#9_Ezra.cpp

---
+++ multiplyLoop/DelaCruz_Act#9_Ezra.cpp
@@ -0,0 +1,25 @@
+/*
+
+Dela Cruz Jan Emmanuel
+11 - Ezra
+Act 9
+5/4/23
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num;
+    
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    for(int i = 1; i <= 10; i++){
+    	cout << i << " x " << num << " = " << i * num << endl;
+	}
+
+    return 0;
+}
